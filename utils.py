4bcb13c50144c274f9119f7217bfd5d05ba3593a
Simone Checchia <techkek@users.noreply.github.com>
2024-08-20 23:52:29 +0200

    chore: Update send_long_message function to disable web page preview

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from config import ENV
+
 def send_long_message(bot, chat_id, text, parse_mode=None):
     max_message_length = 4096
     start = 0
@@ -10,13 +12,24 @@ def send_long_message(bot, chat_id, text, parse_mode=None):
             if end == -1:
                 end = start + max_message_length
         try:
-            bot.send_message(chat_id, text[start:end], parse_mode=parse_mode)
+            bot.send_message(chat_id, text[start:end], parse_mode=parse_mode, disable_web_page_preview=True)
         except Exception as e:
-            print(f"Errore nell'invio del messaggio: {e}")
+            if ENV == "development" or ENV == "debug":
+                print(f"Error sending message: {e}")
             cleaned_text = text[start:end].replace('_', '').replace('*', '').replace('`', '').replace('[', '').replace(']', '')
-            bot.send_message(chat_id, cleaned_text)
+            bot.send_message(chat_id, cleaned_text, parse_mode=parse_mode, disable_web_page_preview=True)
 
         start = end
         
         if start < len(text) and text[start] == '\n':
             start += 1
+
+def format_listening_time(ms):
+    seconds = ms // 1000
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    if hours > 0:
+        return f"{hours}h {minutes}m"
+    else:
+        return f"{minutes}m {seconds}s"
