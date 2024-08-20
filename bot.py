78d41f9322f23a876b754c5ea4e4227c4f421a2c
Simone Checchia <techkek@users.noreply.github.com>
2024-08-20 23:49:59 +0200

    chore: Add flask configurations to bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,27 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
 import telebot
-from config import BOT_TOKEN
+from config import BOT_TOKEN, HOST, PORT, HOST_LOCAL, PORT_LOCAL, ENV
 from commands import register_commands
+import threading
+from callback import app as flask_app
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+def run_flask():
+    if ENV == "production":
+        flask_app.run(host=HOST, port=PORT)
+    else:
+        flask_app.run(host=HOST_LOCAL, port=PORT_LOCAL)
+
+
 if __name__ == "__main__":
     register_commands(bot)
+
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
     bot.polling()
